2404e74a377f2d3f59a02f0969a68eb9faee7961
Hashir Shahzad <hashir447079@gmail.com>
2025-07-21 08:11:29 +0500

    We got Bongo Nvim

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    }
+  end,
+}
