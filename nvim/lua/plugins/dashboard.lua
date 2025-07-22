a0661f54ffb01737fddc79c20196af3ce483c58e
Hashir Shahzad <hashir447079@gmail.com>
2025-07-22 07:46:04 +0500

    dad

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -16,5 +16,15 @@ return {
       "     ███  ██ ██  ██  ██  ██ ██  ██  ██",
       "     ███   ████   ████   ██ ██      ██",
     }
+    -- Footer
+    local stats = require("lazy").stats()
+    local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
+    opts.section.footer.val = "BongoNvim loaded "
+      .. stats.loaded
+      .. "/"
+      .. stats.count
+      .. " plugins  in "
+      .. ms
+      .. "ms"
   end,
 }
